ccfd1bb55838d6f62cb890b0ba1aa9b74d0472b3
kimhun55 <kimhun55@gmail.com>
2017-09-12 16:32:23 +0700

    bass M

--- function.php
+++ function.php
@@ -1,26 +1,5 @@
 <?php 
 function print_bass(){
     echo "bass";
-<<<<<<< HEAD
-}
-=======
-}
-<<<<<<< HEAD
-function print_kaab(){
-    echo "kaab";
 }
 
-=======
-    function print_phat(){
-        echo "phat";
-    }
-        function print_bill(){
-            echo "bill";
-}
->>>>>>> 61da1b960cda01683a139fbd1c018c214649f375
->>>>>>> 4d7873d3ed7781623e3a8865eb71f9e38e616c0f
-<?php 
-function print_nut(){
-    echo "nut";
-    
-}
